3d4f8bdd7860139cac2c83d26a2686bed2f59933
SecondAcNo <gyou.isimura.sigoto@gmail.com>
2025-09-19 09:33:59 +0900

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 function App() {
   const [word, setWord] = useState("");
   
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    alert(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    .then((res) => res.json())
+    .then((data) => {
+      alert(JSON.stringify(data, null, 2));
+    })
+    .catch((err) => {
+      console.log(err);
+    })
   };
 
   return (
